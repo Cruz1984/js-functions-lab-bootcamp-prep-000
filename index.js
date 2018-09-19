539961845766799623de53fc86627d24a0f423e1
MARY <mcruz2946@gmail.com>
2018-09-19 03:12:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+function sayhappyholidays()
+{console.log("happy holidyas")}
+function sayhappy holidyasTo(firstname) {console.log('happy holidays, ${firstname}!');
+function holidayCountdown
+function saycountdown(){console.log('its ${days}dasysuntil${holiday}!')
 // write your code below!
